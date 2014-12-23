7cc442ba2a144fc58d69c969218e4798af12808c
Adam Krone <krone.adam@gmail.com>
2014-12-23 14:33:16 -0600

    Uses wordpress-cluster cookbook for web_production

--- cookbooks/chef-bedrock/recipes/web_production.rb
+++ cookbooks/chef-bedrock/recipes/web_production.rb
@@ -8,45 +8,10 @@
 #
 
 include_recipe 'apt::default'
-include_recipe "git::default"
+include_recipe 'git::default'
 
-capistrano_user 'deploy' do
-  group 'deploy'
-  group_id 3000
-end
-
-include_recipe 'capistrano-base::ssh'
-
-capistrano_wordpress_app 'bedrock' do
-  deploy_root '/var/www/bedrock'
-  docroot '/var/www/bedrock/current/web'
-  deployment_user 'deploy'
-  deployment_group 'deploy'
+wordpress_cluster_app 'bedrock' do
   server_name 'bedrock.dev'
 end
 
-capistrano_shared_file '.env.ctmpl' do
-  template '.env.ctmpl.erb'
-  deploy_root '/var/www/bedrock'
-  owner 'deploy'
-  group 'deploy'
-end
-
-include_recipe "ssh-import-id::default"
-
-include_recipe "consul::default"
-include_recipe "consul-services::apache2"
-include_recipe "consul-services::consul-template"
-
-node.normal['consul_template'] = {
-  consul: '127.0.0.1:8500'
-}
-
-include_recipe "consul-template::default"
-
-consul_template_config 'bedrock_env' do
-  templates [{
-    source: '/var/www/bedrock/shared/.env.ctmpl',
-    destination: '/var/www/bedrock/shared/.env'
-  }]
-end
+include_recipe 'ssh-import-id::default'
