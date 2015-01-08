810b6722d17c1a95a6c0d3773ed3b4785301abd1
Adam Krone <krone.adam@gmail.com>
2015-01-08 15:45:42 -0600

    Updates wordpress-cluster cookbook
    Removes auth keys/salts from dev .env file, moving them to consul
    keys in the terraform config

--- terraform/consul.tf
+++ terraform/consul.tf
@@ -45,6 +45,54 @@ resource "consul_keys" "bedrock" {
     path = "bedrock/wp_siteurl"
     value = "http://bedrock.stg/wp"
   }
+
+  key {
+    name = "auth_key"
+    path = "bedrock/auth_key"
+    value = "J[!%fM.n6b<P~Mcnui;PU6XveFr<<#@wWH/1-+^M=3>_b+;v|+@UXu^20,PN6m!o"
+  }
+
+  key {
+    name = "secure_auth_key"
+    path = "bedrock/secure_auth_key"
+    value = "U7&AZR$Rg1zhSuDSlb?9w%x 7$wz}=OG0q7l7G2%,>_Mil+um~`I3orx<NX$G/g="
+  }
+
+  key {
+    name = "logged_in_key"
+    path = "bedrock/logged_in_key"
+    value = "n$d_f6V|UCM3OD]0t%; >mg&Z3P+0V]%u>VAUcAbF$5)dPGwqCffOl_a^w=B(&c|"
+  }
+
+  key {
+    name = "nonce_key"
+    path = "bedrock/nonce_key"
+    value = "4XAl00n+A(@DueZLG4Q-&x_RU$)zP_ygEYs`lG LlT%FYA*`+t@7RSl/r`w7@V%c"
+  }
+
+  key {
+    name = "auth_salt"
+    path = "bedrock/auth_salt"
+    value = "f,,rz^3KvE&)4T9V#Gpp{AndqK!>+L`/E[o4Z*a-h&Z#wut;1JlM9yQ}8m%0Y5X~"
+  }
+
+  key {
+    name = "secure_auth_salt"
+    path = "bedrock/secure_auth_salt"
+    value = "P#TaQT-YwFwS0QNo{Y1U[dHSqhe%$23oAio&2.A]AWD<|*b]C3Fl6#3[-8YTwo8W"
+  }
+
+  key {
+    name = "logged_in_salt"
+    path = "bedrock/logged_in_salt"
+    value = "w z{*Z!ei|+h*,=fWxqp;-|&OXw!@`t.$S)ReQVnQNvtHny`KPfGvei!2FHzv0t."
+  }
+
+  key {
+    name = "nonce_salt"
+    path = "bedrock/nonce_salt"
+    value = "f|MkP,x+HSk%5J4c{!Ih^R*D k2d@ ?*_L<wEko#2zMu8cwT#40NhE&`PF.mn,~."
+  }
 }
 
 resource "consul_keys" "haproxy" {
