61fb1bbfcc1253e643f8238913066be1713a66d7
Adam Krone <krone.adam@gmail.com>
2015-01-12 16:28:12 -0600

    Update bedrock

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,11 +7,9 @@ set :deploy_to, '/var/www'
 
 set :log_level, :info
 
-# Apache users with .htaccess files:
-# it needs to be added to linked_files so it persists across deploys:
-# set :linked_files, %w{.env web/.htaccess}
-set :linked_files, %w{.env}
-set :linked_dirs, %w{web/app/uploads}
+# set :linked_files, fetch(:linked_files, []).push('.env', 'web/.htaccess')
+set :linked_files, fetch(:linked_files, []).push('.env')
+set :linked_dirs, fetch(:linked_dirs, []).push('web/app/uploads')
 
 namespace :deploy do
   desc 'Restart application'
@@ -26,3 +24,29 @@ end
 # The above restart task is not run by default
 # Uncomment the following line to run it on deploys if needed
 # after 'deploy:publishing', 'deploy:restart'
+
+namespace :deploy do
+  desc 'Update WordPress template root paths to point to the new release'
+  task :update_option_paths do
+    on roles(:app) do
+      within fetch(:release_path) do
+        if test :wp, :core, 'is-installed'
+          [:stylesheet_root, :template_root].each do |option|
+            # Only change the value if it's an absolute path
+            # i.e. The relative path "/themes" must remain unchanged
+            # Also, the option might not be set, in which case we leave it like that
+            value = capture :wp, :option, :get, option, raise_on_non_zero_exit: false
+            if value != '' && value != '/themes'
+              execute :wp, :option, :set, option, fetch(:release_path).join('web/wp/wp-content/themes')
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+# The above update_option_paths task is not run by default
+# Note that you need to have WP-CLI installed on your server
+# Uncomment the following line to run it on deploys if needed
+# after 'deploy:publishing', 'deploy:update_option_paths'
