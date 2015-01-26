2e9029ab71d0c703c4495835dc0208bd8f204b25
Adam Krone <krone.adam@gmail.com>
2015-01-26 11:50:35 -0600

    Removes extra server from staging deploy config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,5 @@ set :stage, :staging
 
 server '172.20.10.11', user: 'deploy', roles: %w{web app}
 server '172.20.10.12', user: 'deploy', roles: %w{web app}
-server '172.20.10.13', user: 'deploy', roles: %w{web app}
 
 fetch(:default_env).merge!(wp_env: :staging)
