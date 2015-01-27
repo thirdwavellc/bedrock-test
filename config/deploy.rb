4cfac467f2f41eea704b4bd9cc740a6bffef8c06
Adam Krone <krone.adam@gmail.com>
2015-01-26 21:54:07 -0600

    Always update theme paths, and only run on first server

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,17 +28,14 @@ end
 namespace :deploy do
   desc 'Update WordPress template root paths to point to the new release'
   task :update_option_paths do
-    on roles(:app, in: :sequence, wait: 5) do
+    on roles(:app).first do
       within fetch(:release_path) do
         if test :wp, :core, 'is-installed'
           [:stylesheet_root, :template_root].each do |option|
             # Only change the value if it's an absolute path
             # i.e. The relative path "/themes" must remain unchanged
             # Also, the option might not be set, in which case we leave it like that
-            value = capture :wp, :option, :get, option, raise_on_non_zero_exit: false
-            if value != '' && value != '/themes'
-              execute :wp, :option, :set, option, fetch(:release_path).join('web/wp/wp-content/themes')
-            end
+            execute :wp, :option, :set, option, fetch(:release_path).join('web/wp/wp-content/themes')
           end
         end
       end
