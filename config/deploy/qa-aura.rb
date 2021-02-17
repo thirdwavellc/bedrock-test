5aaeed3e544c60c6f7a6105b4c931034945ff347
Adam Krone <krone.adam@gmail.com>
2021-02-17 10:15:36 -0600

    Update QA deploy config

--- config/deploy/qa-aura.rb
+++ config/deploy/qa-aura.rb
@@ -1,4 +1,4 @@
 set :deploy_to, '/var/www/bedrock-test'
 
-server 'php01.qa.aura.thirdwave.local', user: 'deploy', roles: %w{web app}
-server 'php02.qa.aura.thirdwave.local', user: 'deploy', roles: %w{web app}
+server 'php01.qa.aura.chicago.thirdwave.3whst.com', user: 'deploy', roles: %w{web app}
+server 'php02.qa.aura.chicago.thirdwave.3whst.com', user: 'deploy', roles: %w{web app}
